0b7044b3ccce43ca20de008acc6f3b849fc9a0b1
Picione <picionevn@gmail.com>
2017-04-19 10:20:40 +1000

    Change sp indicator

--- discord_bot.js
+++ discord_bot.js
@@ -55,7 +55,7 @@ try {
 	console.log("Error in loading SP function.\n"+e.stack);
 };
 // Load Variables
-var alphabet = [":regional_indicator_a:", ":regional_indicator_b:", ":regional_indicator_c:", ":regional_indicator_d:", ":regional_indicator_e:", ":regional_indicator_f:", ":regional_indicator_g:", ":regional_indicator_h:", ":regional_indicator_i:", ":regional_indicator_j:", ":regional_indicator_k:", ":regional_indicator_l:", ":regional_indicator_m:", ":regional_indicator_n:", ":regional_indicator_o:", ":regional_indicator_p:", ":regional_indicator_q:"];
+var alphabet = ["**A** ", "**B** ", "**C** ", "**D** ", "**E** ", "**F** ", "**G** ", "**H** ", "**I** ", "**J** ", "**K** ", "**L** ", "**M** ", "**N** ", "**O** ", "**P** ", "**Q** "];
 
 var itemGLurl = "http://picione.github.io/bravefrontier_data/items.json";
 
